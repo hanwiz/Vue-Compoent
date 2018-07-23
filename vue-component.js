d4c550cbd10dae80662b710b64837ff5a40ab670
Hanwiz <hanwiz@gmail.com>
2018-07-23 17:04:20 -0400

    Add shift with UISlider

--- vue-component.js
+++ vue-component.js
@@ -78,7 +78,7 @@ Vue.component('percentage-input', {
             </div>`
 });
 
-new Vue({
+var sim = new Vue({
     el: '#test',
     data: {
         counter: [50, 50, 50],
@@ -103,5 +103,59 @@ new Vue({
             }
         },
         sliderValues: [[3, 6],[1,7], [2,5]],
-    }
+        intervalPhases: [[0, 1], [1, 7], [7, 8]],
+    },
+    mounted: function () {
+        for (let i = 1; i <= 3; i++) {
+            console.log(this.$refs["interval-" + i]);
+            noUiSlider.create(this.$refs["interval-" + i], {
+                start: [this.intervalPhases[i - 1][0], this.intervalPhases[i - 1][1]],
+                step: 1,
+                range: {
+                    'min': 0,
+                    'max': 8
+                },
+                connect: true,
+                pips: {
+                    mode: 'steps',
+                    values: [0, 8],
+                    filter: function (value, type) {
+                        return value * 2 % 2 ? 0 : 1;
+                    },
+                    format: wNumb({
+                        decimal: 1
+                    }),
+                    density: 100
+                }
+            });
+
+            this.$refs["interval-" + i].noUiSlider.on('update', (values, handle) => {
+                var val = parseFloat(values[handle]);
+                var tip = this.intervalPhases;
+                if (i > 1 && handle === 0) {
+                    if (tip[i - 2][1] !== val) {
+                        tip[i - 2][1] = val;
+                        setTimeout(() => {
+                            this.$refs["interval-" + (i - 1)].noUiSlider.set(tip[i - 2]);
+                        }, 100);
+                    }
+                }
+                if (i < 3 && handle === 1) {
+                    if (tip[i][0] !== val) {
+                        tip[i][0] = val;
+                        //console.log(this.$refs["interval-" + (i+1)][0], tip[i]);
+                        setTimeout(() => {
+                            this.$refs["interval-" + (i + 1)]
+                                .noUiSlider.set(tip[i]);
+                        }, 100);
+                    }
+                }
+                tip[i - 1].splice(handle, 1, val);
+            });
+        }
+        // left-slider of first slider disabled
+        this.$refs["interval-1"].getElementsByClassName("noUi-origin")[0].setAttribute('disabled', true);
+        // right-slider of last slider disabled
+        this.$refs["interval-3"].getElementsByClassName("noUi-origin")[1].setAttribute('disabled', true);
+	}
 });
